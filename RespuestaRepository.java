da8ee2e573d30e9cf5fe49dc748f8c3c3dc1843f
hnavarroley <hugemanhugo@gmail.com>
2025-01-21 19:36:07 -0500

    Create RespuestaRepository.java

---
+++ RespuestaRepository.java
@@ -0,0 +1,7 @@
+package com.alura.foroapi.repository;
+
+import com.alura.foroapi.entity.Respuesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+}
